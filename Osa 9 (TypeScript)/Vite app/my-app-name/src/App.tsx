a89d13e45e7b8b36f7132b86ebecf4729c428bfa
Anssi Pänkäläinen <pankalai@csc.fi>
2025-11-19 11:08:27 +0200

    Exercise 9.16

--- Osa 9 (TypeScript)/Vite app/my-app-name/src/App.tsx
+++ Osa 9 (TypeScript)/Vite app/my-app-name/src/App.tsx
@@ -1,32 +1,86 @@
 interface HeaderProps {
   courseName: string;
 }
-
-interface ContentProps {
-  courseParts: {
-    name: string;
-    exerciseCount: number;
-  }[];
-}
-
 interface TotalProps {
   totalExercises: number;
 }
+
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+interface CoursePartBasicWithDescription extends CoursePartBase {
+  description: string;
+}
+interface CoursePartBasic extends CoursePartBasicWithDescription {
+  kind: "basic"
+}
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+interface CoursePartBackground extends CoursePartBasicWithDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+interface CoursePartSpecial extends CoursePartBasicWithDescription {
+  requirements: string[];
+  kind: "special"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
 
 const Header = (props: HeaderProps) => {
   return <h1>{props.courseName}</h1>;
 }
 
-const Content = (props: ContentProps) => {
+const Content = (props: { courseParts: CoursePart[] }) => {
   return (
     <div>
       {props.courseParts.map(part => (
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
+        <Part key={part.name} {...part} />
       ))}
     </div>
   );
+}
+
+const Part = (props: CoursePart) => {
+    switch (props.kind) {
+      case "basic":
+        return (
+          <p>
+            <b>{props.name} {props.exerciseCount}</b><br></br>
+            {props.description}
+          </p>
+        );
+      case "group":
+        return (
+          <p>
+            <b>{props.name} {props.exerciseCount}</b><br></br>
+            Project exercises: {props.groupProjectCount}
+          </p>
+        );
+      case "background":
+        return (
+          <p>
+            <b>{props.name} {props.exerciseCount}</b><br></br>
+            {props.description}<br></br>
+            Background material: {props.backgroundMaterial}
+          </p>
+        );
+      case "special":
+        return (
+          <p>
+            <b>{props.name} {props.exerciseCount}</b><br></br>
+            {props.description}<br></br>
+            Required skills: {props.requirements.join(", ")}
+          </p>
+        );
+      default:
+        return null;
+    }
 }
 
 const Total = (props: TotalProps) => {
@@ -39,18 +93,45 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
